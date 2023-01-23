60e7a249405bdad1c32fcab2a0464da7ef27e6d7
Ahmed Fawzy <ahmed_ehab03@outlook.com>
2023-01-23 17:32:05 -0500

    RESTful methods update

--- cmd/routes.go
+++ cmd/routes.go
@@ -3,7 +3,7 @@ package main
 import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/AhmedFawzy0/TO-DO/handlers"
-
+	
 )
 
 func setupRoutes(app *fiber.App) {
@@ -13,9 +13,8 @@ func setupRoutes(app *fiber.App) {
 	app.Post("/user", handlers.CreateUser)
 	app.Get("/taskPage", handlers.HandleTaskPage)
 	app.Post("/addTask", handlers.AddTask)
-	app.Get("/signOut",handlers.SignOut)
-	app.Post("/addTask",handlers.AddTask)
-	app.Post("/deleteTask",handlers.DeleteTask)
-	app.Post("/updateTask",handlers.UpdateTask)
+	app.Get("/signOut", handlers.SignOut)
+	app.Delete("/deleteTask", handlers.DeleteTask)
+	app.Put("/updateTask", handlers.UpdateTask)
 
 }
